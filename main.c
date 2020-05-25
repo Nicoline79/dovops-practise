7d3bc5fb2a21a6e5a0e40dea962700f1ffbd891e
root <root@ip-172-31-19-179.us-east-2.compute.internal>
2020-05-25 02:18:18 +0000

    initial commit

---
+++ main.c
@@ -0,0 +1,26 @@
+pseudo code
+{
+Initial Function()
+{
+  //Sample Code
+}
+
+Initial Security()
+{
+ //Sample Code
+<<<<<<< HEAD
+}   
+fearture(){
+//feature1 added
+
+security patch(){
+//security patch added
+
+}
+feature2(){
+//feature2 added
+ feature2
+}
+
+
+}
